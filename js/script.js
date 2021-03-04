0281c2afa07f17633aad5f5181df73c101219b52
KatrineBirk-create <71006793+KatrineBirk-create@users.noreply.github.com>
2021-03-04 13:20:14 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* The Video atomatic start and stop when scrolling */
 var blooper = document.getElementById("myVideo");
 document.addEventListener("scroll", function myFunction(){
     if (blooper.getBoundingClientRect().bottom < 250 || blooper.getBoundingClientRect().top > 250)
@@ -6,10 +7,10 @@ document.addEventListener("scroll", function myFunction(){
         blooper.play();
 });
 
+/* shows opening hours on the day*/
+
 var today = new Date();
 var weekdays = new Array(7);
-var minutesNow = today.getMinutes();
-var hoursNow = today.getHours();
 var greeting = "";
 
 weekdays[0] = "Vi leverer fra på torsdag kl 17";
@@ -21,15 +22,9 @@ weekdays[5] = "Vi leverer i dag fra 17-02";
 weekdays[6] = "Vi leverer i dag fra 17-02";
 
 
-if(weekdays >= 0 && weekdays <=3)
-    greeting = "God!";
-else if(weekdays >= 4 && weekdays <=6)
-    greeting = "God hat!";
-
-
-
     document.getElementById("demo").innerHTML = weekdays[today.getDay()];
 
+/* Click on element to new piture */ 
 function toggleElementVisibility(idOfElement) {
 
     let elementToToggle = document.getElementById(idOfElement);
